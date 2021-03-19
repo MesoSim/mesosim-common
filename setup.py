413aaa21a83c37424f33b566f24e8e354ab45f04
Jon Thielen <github@thielen.science>
2021-03-19 13:36:25 -0500

    Add setup.py to allow install

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2021 MesoSim Developers.
+# Distributed under the terms of the Apache License, Version 2.0.
+# SPDX-License-Identifier: Apache-2.0
+"""Setup script for installing OpenMosaic."""
+
+from setuptools import Extension, setup
+
+setup(
+    use_scm_version={'version_scheme': 'post-release'},
+)
+
